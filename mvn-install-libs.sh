f43d6ea4202608e56b0ef19e4fd267d4947b2590
mpawluczuk <mpawluczuk@bluesoft.net.pl>
2015-03-04 15:02:52 +0100

    initial

---
+++ mvn-install-libs.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=lib/flashselenium-java-client-extension-1.0.jar -DgroupId=com.thoughtworks.selenium -DartifactId=flashselenium-java-client-extension -Dversion=1.0 -Dpackaging=jar
+
